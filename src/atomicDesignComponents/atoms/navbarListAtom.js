af4c49f4074f2365fa56546b0fa51299d5d08db0
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-07-13 22:49:08 +0200

    added navbarListAtom

---
+++ src/atomicDesignComponents/atoms/navbarListAtom.js
@@ -0,0 +1,35 @@
+function NavbarListAtom() {
+  return (
+    <>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Our Screens
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Schedule
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Movie Library
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Location &amp; Contact
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default NavbarListAtom;
