4f20aa20c3fc6b781d14338e84051742e0aba0e7
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-08-14 08:47:46 +0200

    updated close btn

--- src/atomicDesignComponents/atoms/closeButton/closeButton.js
+++ src/atomicDesignComponents/atoms/closeButton/closeButton.js
@@ -4,7 +4,7 @@ function CloseButton(prop) {
   return (
     <>
       <button
-        className="btn-close btn-close-white btn-close-custom"
+        className={"btn-close btn-close-white " + prop.customClass}
         aria-label="Close"
         onClick={prop.onClickHandler}
       ></button>
