ce0fe48ba01809e67d2edadb92a1182b2067b512
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-08-23 21:11:03 +0200

    deleted renamed files

--- src/__tests__/app.test.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from "enzyme";
-import App from "../app";
-
-describe("app component", () => {
-  it("should render app", () => {
-    const wrapper = shallow(<App />);
-  });
-});
