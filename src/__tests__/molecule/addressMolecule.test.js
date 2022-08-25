633d4f8cc442bd281154320101ddf7c8591bfd98
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-08-25 20:05:53 +0200

    addressMolecule test case updated

---
+++ src/__tests__/molecule/addressMolecule.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { shallow } from "enzyme";
+import AddressMolecule from "./../../atomicDesignComponents/molecules/addressMolecule";
+
+describe("AddressMolecule", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<AddressMolecule />);
+  });
+
+  it("should render div with class", () => {
+    console.log(wrapper.debug());
+    const div = wrapper.find("div");
+    expect(div.prop("className")).toEqual(
+      "col-lg-3 col-md-6 col-sm-12 text-md-start text-sm-center"
+    );
+  });
+
+  it("should render first span", () => {
+    const span = wrapper.find("span").at(0);
+    expect(span.text()).toEqual("C. Salvador de Madariaga, 1");
+  });
+
+  it("should render second span", () => {
+    const span = wrapper.find("span").at(1);
+    expect(span.text()).toEqual("28027 Madrid");
+  });
+
+  it("should render third span", () => {
+    const span = wrapper.find("span").at(2);
+    expect(span.text()).toEqual("Spain");
+  });
+});
--- src/atomicDesignComponents/molecules/addressMolecule.js
+++ src/atomicDesignComponents/molecules/addressMolecule.js
@@ -6,9 +6,9 @@ const AddressMolecule = () => {
       <div className="col-lg-3 col-md-6 col-sm-12 text-md-start text-sm-center">
         <h6>Random IT Group</h6>
         <address>
-          <span className="d-block">C. Salvador de Madariaga, 1 </span>
+          <span className="d-block">C. Salvador de Madariaga, 1</span>
           <span className="d-block">28027 Madrid</span>
-          <span className="d-block"> Spain</span>
+          <span className="d-block">Spain</span>
         </address>
       </div>
     </>
